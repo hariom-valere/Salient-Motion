10d28a2f11d65b7d26dfb3f20889f7508caea968
Ganesh V. <ganesh@valere.io>
2025-09-09 18:56:42 +0530

    device connection flow design

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import logo from '@renderer/assets/logo.svg'
+import minimizeIcon from '@renderer/assets/minimize.svg';
+import maximizeIcon from '@renderer/assets/maximize.svg';
+import closeIcon from '@renderer/assets/close.svg';
 
 const TitleBar: React.FC = () => {
   const handleMinimize = () => {
@@ -19,7 +22,17 @@ const TitleBar: React.FC = () => {
       <div className="flex items-center space-x-2 draggable">
         <img src={logo} alt="logo" className=" w-36 h-12 m-auto" />
       </div>
-   
+      <div className="flex space-x-4">
+        <button onClick={handleMinimize} className="w-4 h-4 flex items-center justify-center">
+          <img src={minimizeIcon} alt="minimize" className="w-3 h-3" />
+        </button>
+        <button onClick={handleMaximize} className="w-4 h-4 flex items-center justify-center">
+          <img src={maximizeIcon} alt="maximize" className="w-3 h-3" />
+        </button>
+        <button onClick={handleClose} className="w-4 h-4 flex items-center justify-center">
+          <img src={closeIcon} alt="close" className="w-3 h-3" />
+        </button>
+      </div>
     </div>
   );
 };
