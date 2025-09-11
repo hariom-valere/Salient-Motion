0b1091ae779882c07ad5d5316a03977b5b3d63ec
Ganesh V. <ganesh@valere.io>
2025-09-11 16:25:07 +0530

    Connecting to Device page workng

--- src/renderer/src/pages/connection/index.tsx
+++ src/renderer/src/pages/connection/index.tsx
@@ -6,25 +6,12 @@ import DeviceInfo from './DeviceInfo'
 
 const Connection: React.FC = () => {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col items-center justify-center h-screen bg-[#04100B] text-white p-4">
-      <span className="text-6xl mb-6">🔌</span> {/* Placeholder for the device not connected icon */}
-      <h1 className="text-4xl font-bold mb-4">Device not connected</h1>
-      <p className="text-md text-gray-800 mb-8 text-center">
-        Please connect a device to your account to start managing your network connections.
-      </p>
-      <button className="bg-[#1F7550] text-black font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-[#2ECC71] transition-colors duration-200 text-white">
-        Connect Device
-      </button>
-    </div>
-=======
     <>
-      <ConnectDevice />
+      {/* <ConnectDevice /> */}
       <ConnectingDevice />
-      <ConnectedDevice />
-      <DeviceInfo />
+      {/* <ConnectedDevice /> */}
+      {/* <DeviceInfo /> */}
     </>
->>>>>>> 59e115a67bf53dab3f7951599d2cfb8ccb4b44fe
   )
 }
 
