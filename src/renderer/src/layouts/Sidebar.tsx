c25866c6191c9fbc3f60b6be87ede8630d255aac
hariom-valere <hariomsharma@valere.io>
2025-09-11 19:23:26 +0530

    feat: logic updates

--- src/renderer/src/layouts/Sidebar.tsx
+++ src/renderer/src/layouts/Sidebar.tsx
@@ -86,7 +86,7 @@ const Sidebar: React.FC = () => {
         >
           <img src={DeviceIcon} alt="Device Icon" className="w-5 h-5" />
           {!isCollapsed && <span className="ml-3">Device</span>}
-          {location.pathname === '/connection' && (
+          {!isCollapsed && (
             <span className="ml-auto w-[16px] h-[16px] bg-[#BD1A1D] rounded-full border border-2"></span>
           )}
         </Link>
